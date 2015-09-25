9dd99bc81c94847eb4a3270e41776d07bb71b0f7
Julian Garcia Castillo <squeleton@gmail.com>
2015-09-25 13:32:59 +0200

    Usada funcion pow()

--- Tema2/ejer09/volumenCono.php
+++ Tema2/ejer09/volumenCono.php
@@ -8,7 +8,7 @@
     <?php
     $radio = $_GET['radio'];
     $altura = $_GET['altura'];
-    $volumenCono = (pi()* $radio*$radio*$altura)/3;
+    $volumenCono = (pi()* pow($radio, 2)*$altura)/3;
     ?>
     Volumen del cono: <?php echo $volumenCono ?><br>
   </body>
