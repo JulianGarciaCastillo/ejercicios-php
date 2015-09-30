de12f45d78eae2394d31b4b87884ff04e6a8d306
Julian Garcia Castillo <squeleton@gmail.com>
2015-09-30 12:41:12 +0200

    Cambio visual

--- Tema4/ejer07/index.php
+++ Tema4/ejer07/index.php
@@ -3,17 +3,11 @@
 <html>
   <head>
     <meta charset="UTF-8">
+    <link href="hoja.css" rel="stylesheet" type="text/css">
     <title></title>
   </head>
   <body>
-    <b>7. Realiza el control de acceso a una caja fuerte. La combinación será un número de 4 cifras. El
-programa nos pedirá la combinación para abrirla. Si no acertamos, se nos mostrará el mensaje
-“Lo siento, esa no es la combinación” y si acertamos se nos dirá “La caja fuerte se ha abierto
-satisfactoriamente”. Tendremos cuatro oportunidades para abrir la caja fuerte.</b>
     <br><br>
-    <b>BLACKBOX V1.0</b><br><br>
-
-
 <?php $passSecreta = 1234; ?>
 
 <?php
@@ -29,16 +23,15 @@ if (!(isset($_POST['contador']))){
   $passUser = $_POST['passUser'];
 }
 
-if ($passSecreta == $passUser) {
-  echo "¡ Has acertado el codigo !";
-}else if ($contador == 0){
-  echo "No te quedan más intentos.";
-}else{
-  echo "Te quedan ", $contador, " intentos para abrir la caja. <br>";
+if ($passSecreta == $passUser) {?>
+ <div id="mensajeFinalsi"> ACCESO GARANTIZADO</div><?php
+}else if ($contador == 0){?>
+ <div id="mensajeFinalno"> ACCESO DENEGADO</div><?php
+}else{?>
+ <div id="mensajeIntentos"> Te quedan <?php echo $contador ?> intentos.</div><?php 
   $contador--;
-  echo "Introduce un número de cuatro cifras.";
     echo '<form action="index.php" method="post">';
-    echo '<input type="number" min=0 max=9999 name="passUser">';
+    echo '<input type="number" min=0 max=9999 name="passUser" autofocus placeholder="- - - -">';
     echo '<input type="hidden" name="contador" value="', $contador, '">';
     echo '<input type="submit" value="Continuar">';
     echo '</form>';
